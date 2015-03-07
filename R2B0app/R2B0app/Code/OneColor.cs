45848c0c36c2275e6a88d2f711c8bb8d44ab6e62
Stéphane Fardoux <sfardoux@hotmail.com>
2015-03-07 16:29:47 +0100

    2 missing files

---
+++ R2B0app/R2B0app/Code/OneColor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace R2B0app
+{
+    public class OneColor
+    {
+        public int Red = 255;
+        public int Green = 255;
+        public int Blue = 255;
+        public int Alpha = 255; 
+         
+        public Color Color { get { return Color.FromRgba(Red, Green, Blue, Alpha); } }
+    }
+}
---
+++ R2B0app/R2B0app/ViewKeyboard.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace R2B0app
+{
+    public partial class ViewKeyboard : ContentView
+    {
+        private Screen screen = Screen.Main;
+
+        public ViewKeyboard(Screen screen)
+        {
+            InitializeComponent();
+
+            this.screen = screen;
+            switch (screen)
+            {
+                case Screen.Main:
+                    lTitle.Text = "MAIN CONTROL 1";
+                    break;
+                case Screen.Holos:
+                    lTitle.Text = "HOLOS CONTROL 1";
+                    break;
+                case Screen.Logics:
+                    lTitle.Text = "LOGICS CONTROL 1";
+                    break;
+                case Screen.Panel:
+                    lTitle.Text = "PANEL CONTROL 1";
+                    break;
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                AddButton(2 + 4 * (i % 3), 3 + 4 * (i / 3), i.ToString(), "");
+            }
+        }
+
+        private void AddButton(int x, int y, string text, string command)
+        {
+            //theGrid.Children.Add(new Label { Text = text, FontSize = 18, HorizontalOptions = LayoutOptions.Center, VerticalOptions=LayoutOptions.Center }, x, y);
+            theGrid.Children.Add(new Button { Text = text, TextColor = ThemeColors.GetColor("TEXT"), BackgroundColor = ThemeColors.GetColor("FILL"), BorderColor = ThemeColors.GetColor("BORDER"), BorderWidth = 1, FontSize = 18, HorizontalOptions = LayoutOptions.Fill, VerticalOptions = LayoutOptions.FillAndExpand }, x, y);
+        } 
+
+    }
+}
