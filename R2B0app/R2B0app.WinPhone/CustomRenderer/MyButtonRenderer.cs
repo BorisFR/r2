e2bfca8000d2bd70a8c3a65159172a0985b7722c
Stéphane Fardoux <sfardoux@hotmail.com>
2015-03-09 13:46:20 +0100

    CustomRenderer WP + notes

--- R2B0app/R2B0app.WinPhone/CustomRenderer/MyButtonRenderer.cs
+++ R2B0app/R2B0app.WinPhone/CustomRenderer/MyButtonRenderer.cs
@@ -1,17 +1,20 @@
-﻿[assembly: ExportRenderer (typeof (MyButton), typeof (MyButtonRenderer))]
-namespace R2B0app.iOS
+﻿using R2B0app;
+using R2B0app.WinPhone;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.WinPhone;
+
+[assembly: ExportRenderer (typeof (MyButton), typeof (MyButtonRenderer))]
+namespace R2B0app.WinPhone
 {
 	public class MyButtonRenderer : ButtonRenderer
 	{
-		protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
-		{
-			base.OnElementChanged(e);
-
-			if (e.OldElement == null)
-			{
-				//var nativePhoneTextBox = (PhoneTextBox)Control.Children[0];
-				//nativePhoneTextBox.Background = new SolidColorBrush(Colors.Yellow);
-			}
-		}
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            if (Control == null)
+                return;
+            //Control.Margin = new System.Windows.Thickness(0);
+            Control.Padding = new System.Windows.Thickness(0);
+        }
 	}
 }
